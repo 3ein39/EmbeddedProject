dfb2f2c1563b2e70655e98547c5b1b0cefaa7b8c
Hussein Hany <hussein.hany.cs@gmail.com>
2024-03-08 08:23:59 +0200

    Initialize project

---
+++ EmbeddedProject/main.c
@@ -0,0 +1,18 @@
+/*
+ * EmbeddedProject.c
+ *
+ * Created: 08/03/2024 08:09:27
+ * Author : Hussein Hany
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
